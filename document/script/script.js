6783203eb480582ff12fdb55ec38ffb569942b58
Ramis <ramis.gaynetdinov@gmail.com>
2021-03-06 17:20:21 +0000

    upd document'
    :
    
    cd
    :::!

--- document/script/script.js
+++ document/script/script.js
@@ -1,11 +1,15 @@
 const body = document.querySelector('body');
-
-console.log(body);
-
-console.log(body.style);
-
 body.style.backgroundColor = '#696969';
 
 const page = document.querySelector('.page');
+page.style.color = 'ghostwhite';
 
-page.style.backgroundColor = 'ghostwhite';
+const name = document.querySelector('.name');
+name.textContent = 'DJ Rashad';
+
+const link = document.querySelector('img');
+link.setAttribute('src', 'script/DJRashad.jpg');
+
+const bio = document.querySelector('.short-bio');
+bio.className += ' animated';
+
