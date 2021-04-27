8846d8b9661ac30e74055f9c8e95842d0b9b48e9
Ramis <ramis.gaynetdinov@gmail.com>
2021-04-27 06:38:35 +0100

    udalos

--- Crypto/script/script.js
+++ Crypto/script/script.js
@@ -16,54 +16,16 @@ let crypto = [
   }
 ]
 
-let nameOfCrypto = document.querySelector('.name');
-let costOfCrypto = document.querySelector('.cost');
-let colorOfCrypto = document.querySelector('div');
-
 for(currency of crypto){
+    let nameOfCrypto = document.createElement('h2');
+    document.body.appendChild(nameOfCrypto);
+    let costOfCrypto = document.createElement('h2');
+    document.body.appendChild(costOfCrypto);
+    let colorOfCrypto = document.createElement('div');
+    document.body.appendChild(colorOfCrypto);
     nameOfCrypto.innerHTML = currency.name;
     costOfCrypto.innerHTML = currency.price;
     colorOfCrypto.style.backgroundColor = currency.color;
     colorOfCrypto.style.width = currency.price / 20 + '%';
     colorOfCrypto.style.height = '2rem';
 }
-
-// For in в For of (ВЫВОДИТ ТОЛЬКО Bitcoin 3 раза
-// если задать querySelectorAll):
-
-// for(currency of crypto){
-//     for(key in nameOfCrypto){
-//     nameOfCrypto[key].innerHTML = currency.name;
-//   }
-//     for(key in costOfCrypto){
-//     costOfCrypto[key].innerHTML = currency.price;
-//   }
-//     for(key in colorOfCrypto){
-//     colorOfCrypto[key].style.backgroundColor = currency.color;
-//     colorOfCrypto[key].style.width = currency.price / 20 + '%';
-//     colorOfCrypto[key].style.height = '2rem';
-//   } 
-// }
- 
-
-//ПЕРВОЕ РЕШЕНИЕ:
-
-// bitcoin.innerHTML = crypto[0].name;
-// etherium.innerHTML = crypto[1].name;
-// litecoin.innerHTML = crypto[2].name;
-
-// bitcoinCost.innerHTML = crypto[0].price;
-// etheriumCost.innerHTML = crypto[1].price;
-// litecoinCost.innerHTML = crypto[2].price;
-
-// bitcoinColor.style.backgroundColor = '#D0021C'
-// bitcoinColor.style.width = crypto[0].price / 2 + 'px';
-// bitcoinColor.style.height = '2rem';
-
-// etheriumColor.style.backgroundColor = '#4990E2'
-// etheriumColor.style.width = crypto[1].price / 2 + 'px';
-// etheriumColor.style.height = '2rem';
-
-// litecoinColor.style.backgroundColor = '#4FE3C1'
-// litecoinColor.style.width = crypto[2].price / 2 + 'px';
-// litecoinColor.style.height = '2rem';
