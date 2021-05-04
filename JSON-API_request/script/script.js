d3181428be8820873be61cd0f1390d84055b78d1
Ramis <ramis.gaynetdinov@gmail.com>
2021-05-04 20:25:43 +0100

    request

--- JSON-API_request/script/script.js
+++ JSON-API_request/script/script.js
@@ -1,27 +1,23 @@
-
 const form = document.forms;
-const city = document.forms[0].elements.gorod;
+const city = document.forms[0].elements.city.value;
 const APIKey = 'ccbd0fc8abb39c00bd73e9dbf8cf89f6';
 const url = 'http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+APIKey;
 let temp = document.querySelector('.temperature');
 
 let xhr = new XMLHttpRequest();
 
-xhr.open('GET', url, false);
-
-xhr.send();
-
-let DATA = JSON.parse(xhr.responseText);
-
-console.log(DATA);
-
 form.onsubmit = function(e){
 	e.preventDefault();
+
+	xhr.open('GET', url, false);
+
+	xhr.send();
+
+	let DATA = JSON.parse(xhr.responseText);
 
 	if(xhr.status != 200){
 		console.log(xhr.status + ' ' + xhr.statusText);
 	} else{
-		city.innerHTML = form.elements.gorod.value;
 		temp.innerHTML = DATA.main.temp;
 	}
 }
