e1623fbdf24cc6f44505a31813c1aa24a44b72ab
Ramis <ramis.gaynetdinov@gmail.com>
2021-05-10 16:51:09 +0100

    up

--- JSON-API_request/script/script.js
+++ JSON-API_request/script/script.js
@@ -4,10 +4,10 @@ const APIKey = 'ccbd0fc8abb39c00bd73e9dbf8cf89f6';
 const url = 'http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+APIKey;
 let temp = document.querySelector('.temperature');
 
-let xhr = new XMLHttpRequest();
-
 form.onsubmit = function(e){
 	e.preventDefault();
+
+	let xhr = new XMLHttpRequest();
 
 	xhr.open('GET', url, false);
 
@@ -15,10 +15,13 @@ form.onsubmit = function(e){
 
 	let DATA = JSON.parse(xhr.responseText);
 
+	console.log(DATA);
+
 	if(xhr.status != 200){
 		console.log(xhr.status + ' ' + xhr.statusText);
 	} else{
 		temp.innerHTML = DATA.main.temp;
+		
 	}
 }
 
